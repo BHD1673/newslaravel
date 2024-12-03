ab620ddf9d7911a407e5eed55343f34d8792c34c
Dang Thien <dangthien@gmail.com>
2024-12-03 22:21:38 +0700

    chinh sua luong dang bai

--- resources/views/main_layouts/master.blade.php
+++ resources/views/main_layouts/master.blade.php
@@ -224,7 +224,7 @@ $categoryFooter  = Category::where('name','!=','Chưa phân loại')->withCount(
 							<ul class="dropdown-menu">
 								<li><a href="{{ route('about') }}">Giới thiệu</a></li>
 								<li><a href="{{ route('contact.create') }}">Liên hệ</a></li>
-								{{-- <li><a href="{{ route('shop.index') }}">Shop</a></li> --}}
+									<li><a href="{{ route('shop.index') }}">Shop</a></li> 
 								{{-- <li><a href="{{ route('erorrs.404') }}">404</a></li> --}}
 							</ul>
 						</li>
